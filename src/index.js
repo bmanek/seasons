3446287c515059d163fb29d9bc26eee81bc4042e
bmanek <41875460+bmanek@users.noreply.github.com>
2019-09-23 14:31:34 -0400

    add error handling and basic conditional rendering

--- src/index.js
+++ src/index.js
@@ -7,17 +7,39 @@ class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = { lat: null }
+    this.state = {
+      lat: null,
+      errorMessage: ""
+    }
+
+// moved out of render b/c too costly to do multiple times
+    window.navigator.geolocation.getCurrentPosition(
+// getCurrentPosition takes two callbacks: success and failure
+      position => {
+        this.setState({
+          lat: position.coords.latitude
+        })
+      },
+      err => {
+        this.setState({
+          errorMessage: err.message
+        })
+      }
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-        // success
-        (position) => console.log(position),
-        // failure
-        (err) => console.log(err)
-      )
-    return <div>Latitude:</div>
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>
+
+
   }
 }
 
