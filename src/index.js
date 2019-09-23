5d93e91703eb5b5733abd72a86bbaba512124d95
bmanek <41875460+bmanek@users.noreply.github.com>
2019-09-23 10:34:21 -0400

    refactor App to class structure

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    // success
-    (position) => console.log(position),
-    // failure
-    (err) => console.log(err)
-  )
 
-  return(
-    <div>Latitude:</div>
-  )
+// refactor App to class
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+        // success
+        (position) => console.log(position),
+        // failure
+        (err) => console.log(err)
+      )
+    return <div>Latitude:</div>
+  }
 }
+
+
+// original, functional syntax
+
+// const App = () => {
+//   return(
+  //     <div>Latitude:</div>
+  //   )
+  // }
 
 ReactDOM.render(
   <App />,
